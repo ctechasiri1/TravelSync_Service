25c0f9d02e53a39d30fee2198895b329a1626fe6
ctechasiri1 <ctechasiri@gmail.com>
2026-03-23 18:57:00 -0700

    Added the routes of authentication

--- routers/users.py
+++ routers/users.py
@@ -2,9 +2,11 @@ from datetime import timedelta
 from typing import Annotated
 
 from fastapi import APIRouter, Depends, HTTPException, UploadFile, status
+from fastapi.security import OAuth2PasswordRequestForm
 
 from sqlalchemy import func, select
 from sqlalchemy.ext.asyncio import AsyncSession
+
 
 import models
 from auth import (
@@ -13,9 +15,10 @@ from auth import (
     hash_password, 
     verify_password
 )
+from config import settings
 from database import get_db
 from image_utils import process_image, delete_image
-from schemas import UserCreate, UserPrivate, UserPublic, UserUpdate
+from schemas import UserCreate, UserPrivate, UserPublic, UserUpdate, Token
 
 router = APIRouter()
 
@@ -57,3 +60,37 @@ async def create_user(user: UserCreate, db: Annotated[AsyncSession, Depends(get_
     db.add(new_user)
     await db.commit()
     await db.refresh(new_user)
+
+    return new_user
+
+@router.post("/token", response_model=Token)
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: Annotated[AsyncSession, Depends(get_db)]):
+    # looks up the user by email (case-insensitive)
+    # Note: OAuth2PasswordRequestForm uses "username" field, but we treat it as email
+    result = await db.execute(
+        select(models.User)
+        .where(func.lower(models.User.email) == form_data.username.lower())
+    )
+
+    user = result.scalars().first()
+
+    #verify the uesr exists and the password is correct
+    # don't reveal which one failed (security best practice)
+    if not user or not verify_password(form_data.password, user.password_hash):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+
+    #create an access token with the user id as the subject
+    access_token_expire = timedelta(minutes=settings.access_token_expire_minutes)
+    access_token = create_access_token(
+        data={"sub": str(user.id)},
+        expires_delta=access_token_expire
+    )
+
+    return Token(
+        access_token=access_token,
+        token_type="bearer"
+    )
